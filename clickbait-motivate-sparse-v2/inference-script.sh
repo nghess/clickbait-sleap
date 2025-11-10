bdc7d30b77053c7ad7081f46f22a1fd3bd37e75e
nghess <nhess@uoregon.edu>
2025-11-10 01:11:16 -0800

    sleap-nn

---
+++ clickbait-motivate-sparse-v2/inference-script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sleap-nn-track --data_path C:/clickbait-sleap/analysis-rig-test/7012_m11_labels.v001.slp --video_index 3 --frames 79892 --model_paths C:\clickbait-sleap\analysis-rig-test\models\None_251029_182449.single_instance.n=132 -o 7012_m11_labels.v001.slp.predictions.slp --batch_size 4
